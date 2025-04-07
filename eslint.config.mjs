b7c143a81b8ec3468f59beb9a54f154ee419c1cd
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2025-04-07 14:22:20 -0400

    chore: update deps, setup initial nextjs, add semantic release and co… (#12)
    * chore: update deps, setup initial nextjs, add semantic release and commitlint
    
    * chore: add stashed changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,42 +1,16 @@
-import eslintNode from "eslint-plugin-node";
-import eslintPluginUnicorn from "eslint-plugin-unicorn";
-import eslintPrettier from "eslint-config-prettier";
-import eslintTailwind from "eslint-plugin-tailwindcss";
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import sonarjs from "eslint-plugin-sonarjs";
-import sortExports from "eslint-plugin-sort-exports";
-import tseslint from "typescript-eslint";
-import unusedImports from "eslint-plugin-unused-imports";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
-export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  {
-    languageOptions: {
-      parser: "@typescript-eslint/parser",
-      ecmaVersion: 2022,
-      sourceType: "module",
-      globals: { ...globals.browser, ...globals.node },
-    },
-  },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  eslintPrettier,
-  sonarjs.configs.recommended,
-  {
-    plugins: {
-      sortExports: sortExports,
-      unusedImports: unusedImports,
-      node: eslintNode,
-      tailwindcss: eslintTailwind,
-      unicorn: eslintPluginUnicorn,
-      sonar: sonarjs,
-    },
-  },
-  {
-    rules: {
-      "no-undef": "off",
-    },
-  },
-  { ignores: ["node_modules/", ".github/", "*.json"] },
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
+
+export default eslintConfig;
