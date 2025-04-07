0f665378b367c317fe45993867a632b6251e3d1c
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2025-04-07 17:43:53 -0400

    chore: resolve lint error (#13)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 // tailwind.config.js
-const { heroui } = require("@heroui/theme");
+const { heroui } = require('@heroui/theme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./node_modules/@heroui/theme/dist/components/(autocomplete|badge|breadcrumbs|button|card|chip|divider|input|navbar|pagination|select|spinner|toggle|tabs|popover|ripple|form|listbox|scroll-shadow).js",
-  ],
-  theme: {
-    extend: {},
-  },
-  darkMode: "class",
-  plugins: [heroui()],
+    content: [
+        './node_modules/@heroui/theme/dist/components/(autocomplete|badge|breadcrumbs|button|card|chip|divider|input|navbar|pagination|select|spinner|toggle|tabs|popover|ripple|form|listbox|scroll-shadow).js',
+    ],
+    theme: {
+        extend: {},
+    },
+    darkMode: 'class',
+    plugins: [heroui()],
 };
