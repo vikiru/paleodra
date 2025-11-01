6c2e2004b380ef0cd169ebbec50314dfa7ab3415
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2025-11-01 17:26:06 -0400

    feat: create simple script to fetch dinosaur data from backend api (#20)
    * chore: commit recent local changes
    
    * chore: commit recent local changes
    
    * chore: update deps
    
    * chore: update deps
    
    * chore: add additional console log messages

---
+++ src/config/index.ts
@@ -0,0 +1,22 @@
+import path from 'node:path';
+import dotenv from 'dotenv';
+
+import * as z from 'zod';
+
+dotenv.config({ path: path.resolve(process.cwd(), '.env') });
+
+export const envSchema = z.object({
+    API_URL: z.url(),
+    NODE_ENV: z
+        .enum(['development', 'test', 'production'])
+        .default('development'),
+});
+
+export const validatedEnv = envSchema.safeParse(process.env);
+
+if (!validatedEnv.success) {
+    console.error(validatedEnv.error);
+    process.exit(1);
+}
+
+export const { API_URL, NODE_ENV } = validatedEnv.data;
