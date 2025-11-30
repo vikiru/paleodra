d1cd67a7d67d5af6a4e91fd11ef326ca5075d60b
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2025-11-30 17:40:41 -0500

    fix: enable zustand mapset for immer to work with set mutations

--- src/store/dinoStore.ts
+++ src/store/dinoStore.ts
@@ -1,32 +1,35 @@
-import { create } from "zustand";
-import { persist } from "zustand/middleware";
-import { immer } from "zustand/middleware/immer";
-import { createCustomStorage } from "@/store/customStorage";
+import { enableMapSet } from 'immer';
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+import { immer } from 'zustand/middleware/immer';
+import { createCustomStorage } from '@/store/customStorage';
+
+enableMapSet();
 
 type DinoState = {
-	seenIds: Set<number>;
-    getAllSeenIds: () => number[];
-	markAsSeen: (id: number) => void;
-	clearSeenIds: () => void;
+  seenIds: Set<number>;
+  getAllSeenIds: () => number[];
+  markAsSeen: (id: number) => void;
+  clearSeenIds: () => void;
 };
 
 export const useDinoStore = create<DinoState>()(
-	persist(
-		immer((set, get) => ({
-			seenIds: new Set<number>(),
-            getAllSeenIds: () => Array.from(get().seenIds),
-			markAsSeen: (id) =>
-				set((state) => {
-					state.seenIds.add(id);
-				}),
-			clearSeenIds: () =>
-				set((state) => {
-					state.seenIds.clear();
-				}),
-		})),
-		{
-			name: "dino-store",
-			storage: createCustomStorage<DinoState>(),
-		},
-	),
+  persist(
+    immer((set, get) => ({
+      seenIds: new Set<number>(),
+      getAllSeenIds: () => Array.from(get().seenIds),
+      markAsSeen: (id) =>
+        set((state) => {
+          state.seenIds.add(id);
+        }),
+      clearSeenIds: () =>
+        set((state) => {
+          state.seenIds.clear();
+        }),
+    })),
+    {
+      name: 'dino-store',
+      storage: createCustomStorage<DinoState>(),
+    },
+  ),
 );
