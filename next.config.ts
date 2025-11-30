21991908302c9c73f4b414a78e5132a65e5bd257
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2025-11-30 17:37:33 -0500

    chore: configure nextjs to allow images from wikipedia

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'commons.wikimedia.org',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
