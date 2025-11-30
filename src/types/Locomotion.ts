a73e30b81e97d77e967149a2c073a3cdeac42f0a
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2025-11-30 17:39:11 -0500

    chore: update type info

--- src/types/Locomotion.ts
+++ src/types/Locomotion.ts
@@ -1,9 +1,9 @@
 export const OLocomotion = {
-    BIPED: 'biped',
-    FACULTATIVE_BIPED: 'facultative biped',
-    GLIDING: 'gliding',
-    QUADRUPED: 'quadruped',
-    SWIMMING: 'swimming',
+  BIPED: 'biped',
+  FACULTATIVE_BIPED: 'facultative biped',
+  GLIDING: 'gliding',
+  QUADRUPED: 'quadruped',
+  SWIMMING: 'swimming',
 } as const;
 
 export type Locomotion = (typeof OLocomotion)[keyof typeof OLocomotion];
