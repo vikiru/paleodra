4aec97b2dd11e9dde2abbca97217e0656ea38593
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2025-11-30 22:09:16 -0500

    refactor: let filter hook return early if no filters

--- src/components/explore/useFilteredDinosaurs.ts
+++ src/components/explore/useFilteredDinosaurs.ts
@@ -4,6 +4,30 @@ import type { Diet } from '@/types/Diet';
 import type { Dinosaur } from '@/types/Dinosaur';
 import type { Locomotion } from '@/types/Locomotion';
 
+const dinosaursData = dinosaurs as unknown as Dinosaur[];
+
+const filterDinosaurs = (
+  search: string,
+  cladeFilter: string | undefined,
+  dietFilter: Diet | undefined,
+  locomotionFilter: Locomotion | undefined,
+) => {
+  const hasNoFilters =
+    !search && !cladeFilter && !dietFilter && !locomotionFilter;
+  if (hasNoFilters) return dinosaursData;
+
+  return dinosaursData.filter((dino) => {
+    const matchesSearch = !search || dino.name.toLowerCase().includes(search);
+    const matchesClade =
+      !cladeFilter || dino.classificationInfo.clade.includes(cladeFilter);
+    const matchesDiet = !dietFilter || dino.diet === dietFilter;
+    const matchesLocomotion =
+      !locomotionFilter || dino.locomotionType === locomotionFilter;
+
+    return matchesSearch && matchesClade && matchesDiet && matchesLocomotion;
+  });
+};
+
 export function useFilteredDinosaurs(searchParams: {
   [key: string]: string | undefined;
 }): Dinosaur[] {
@@ -13,16 +37,7 @@ export function useFilteredDinosaurs(searchParams: {
     const dietFilter = searchParams.diet as Diet | undefined;
     const locomotionFilter = searchParams.locomotion as Locomotion | undefined;
 
-    return dinosaurs.filter((dino) => {
-      const matchesSearch = dino.name.toLowerCase().includes(search);
-      const matchesClade =
-        !cladeFilter || dino.classificationInfo.clade.includes(cladeFilter);
-      const matchesDiet = !dietFilter || dino.diet === dietFilter;
-      const matchesLocomotion =
-        !locomotionFilter || dino.locomotionType === locomotionFilter;
-
-      return matchesSearch && matchesClade && matchesDiet && matchesLocomotion;
-    });
+    return filterDinosaurs(search, cladeFilter, dietFilter, locomotionFilter);
   }, [
     searchParams.search,
     searchParams.clade,
