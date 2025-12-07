6d84eef2194f90969208f7569c618d73713faf0f
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2025-12-06 20:31:06 -0500

    chore: fix link hover on logo taking up full width instead of content width

--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -1,13 +1,11 @@
-import { Bone } from 'lucide-react';
 import Link from 'next/link';
 
 export function Logo() {
   return (
-    <Link className="flex items-center space-x-2" href="/">
-      <div className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-full bg-primary/10 text-primary">
-        <Bone className="h-5 w-5" />
-      </div>
-      <span className="logo text-2xl font-bold gradient">Paleodra</span>
-    </Link>
+    <section id="logo">
+      <Link href="/">
+        <span className="logo text-2xl font-bold gradient">Paleodra</span>
+      </Link>
+    </section>
   );
 }
